445807823c524ac3095e4ada3de8ee98770fa9c8
kenechi <nsudekenechi2@gmail.com>
2024-01-24 13:19:18 +0100

    Finished Contact Responsiveness

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -10,29 +10,31 @@ export const Contact = () => {
           I strive for the best in everything I create.
         </p>
         <p className='text-3xl'>Let's Get In Touch</p>
-        <form action="" className='py-10'>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-10">
+        <div className='w-10 h-5 border-l border-b border-[#666] my-10'></div>
+        <form action="" className=''>
+          <div className="grid grid-cols-1  md:grid-cols-2 gap-x-10 gap-y-10">
             <div>
               <div className='mb-2 '>
                 <label htmlFor="" className='text-sm'>Name</label>
               </div>
-              <input type="text" className='text-sm  w-[100%] md:py-3 md:px-5 outline-none  bg-[#222] rounded-md' />
+              <input type="text" className='text-sm  w-[100%] py-3 px-5 outline-none  bg-[#222] rounded-md' required/>
             </div>
 
             <div>
               <div className='mb-2 '>
                 <label htmlFor="" className='text-sm'>Email</label>
               </div>
-              <input type="text" className='text-sm  w-[100%] md:py-3 md:px-5 outline-none  bg-[#222] rounded-md' />
+              <input type="text" className='text-sm  w-[100%] py-3 px-5 outline-none  bg-[#222] rounded-md' required/>
             </div>
 
-            <div className='col-span-2 mb-5'>
+            <div className='md:col-span-2 mb-5'>
               <div className='mb-2 flex items-center gap-x-2'>
                 <label htmlFor="" className='text-sm'>Send me a message</label>
               </div>
-              <textarea name="" id="" className='text-sm w-[100%] bg-[#222]  resize-none outline-none md:py-3 md:px-5 rounded-md h-[300px]'></textarea>
+              <textarea name="" id="" className='text-sm w-[100%] bg-[#222]  resize-none outline-none py-3 px-5 rounded-md h-[300px]' required></textarea>
             </div>
           </div>
+          
           <button className='flex gap-x-2 text-sm font-light items-center py-2 px-5 bg-blue-600'>Send Message <MdEmail /></button>
         </form>
 
