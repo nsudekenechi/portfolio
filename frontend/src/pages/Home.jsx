014c78127092b4c1185087304abcd7a04ef2bc97
kenechi <nsudekenechi2@gmail.com>
2024-01-25 10:10:58 +0100

    Started Working On Animation for Skills page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,27 @@ import { motion } from "framer-motion"
 import { Link } from 'react-router-dom';
 export const Home = () => {
     return (
-        <motion.div  initial={{ y: 70, opacity: 0 }} exit={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{duration: 0.4}}>
-            
+        <motion.div initial={{ y: 70, opacity: 0 }} exit={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.4 }}>
+
             <div className='text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 <div className='col-span-2'>
-                    <h1 className=' text-[5rem] md:text-[5rem] lg:text-[6rem] font-bold mb-2 font-Fira  '>Kenechi <br /> <span className='border-b border-b-blue-600 '>N</span>sude.</h1>
-                    <h1 className='text-xl md:mb-10 '>FullStack Developer</h1>
+                    <h1 className=' text-[5rem] md:text-[5rem] lg:text-[6rem] font-bold mb-2 font-Fira  relative'>
+                        <span className='relative'>
+                            Kenechi
+                            <motion.div className=' absolute bg-black  right-0 top-[0%] ' initial={{ width: "100%", height: "100%" }} animate={{ width: "0%" }} transition={{ duration: 0.5 }}></motion.div>
+                        </span>
+                        <br />
+                        <span className='relative'>
+                            <span className='border-b border-b-blue-600 '>N</span>sude.
+                            <motion.div className=' absolute bg-black  left-0 top-[0%] ' initial={{ width: "100%", height: "100%" }} animate={{ width: "0%" }} transition={{ duration: 0.65, ease: "easeInOut" }}></motion.div>
+                        </span>
+
+                    </h1>
+                    <h1 className='text-xl md:mb-10 relative'>
+                        FullStack Developer
+                        <motion.div className=' absolute   left-0 top-[0%] ' initial={{ width: "30%", height: "100%", background: "rgb(37, 99, 235)" }} animate={{ width: "0%", background: ["rgb(37, 99, 235)", "rgb(37, 99, 235)", "rgb(0, 0, 0)"] }} transition={{ duration: 1, ease: "anticipate" }}></motion.div>
+
+                    </h1>
                     <div className='py-10 flex gap-x-5 items-center'>
                         <Link className='flex gap-x-2 items-center hover:-translate-y-0.5 duration-700 hover:text-purple-800 text-sm'>
                             <FaGithub size={20} />
@@ -27,10 +42,28 @@ export const Home = () => {
                         </Link>
                     </div>
                 </div>
-                <div className='font-[poppins]'>
-                    <p className='text-2xl text-[#888] mb-10'>Hello, There 👋</p>
-                    <p className='leading-7'>My name is Kenechi Nsude, a fullstack developer with a flair for building innovative solutions for both individuals and large businesses.
-                        My passion for technology extends beyond coding, driving me to stay abreast of industry trends and consistently exceed client expectations. Let's collaborate to bring your digital vision to life.
+                <div className='font-[poppins] overflow-hidden'>
+                    <motion.p
+                        initial={{ opacity: 0, translateX: "20%" }}
+                        animate={{ opacity: 1, translateX: "0%" }}
+                        transition={{
+                            delay: 0.4,
+                            duration: 0.7,
+                            ease:"anticipate"
+                        }}
+                        className='text-2xl text-[#888] mb-10' >Hello, There 👋</motion.p>
+                    <p className='leading-7 overflow-hidden'>
+                        <motion.p  
+                         initial={{ opacity: 0, translateY: "-100%" }}
+                        animate={{ opacity: 1, translateY: "0%" }}
+                        transition={{
+                            delay: 0.65,
+                            duration: 0.8,
+                            ease:"circOut"
+                        }}>
+                            My name is Kenechi Nsude, a fullstack developer with a flair for building innovative solutions for both individuals and large businesses.
+                        </motion.p>
+                       
                     </p>
                 </div>
             </div>
--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -103,107 +103,127 @@ export const Skills = () => {
         }
     ]
     return (
-        <motion.div initial={{ y: 70, opacity: 0 }} exit={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.4 }}>
-         
+        <motion.div initial={{ y: 100, opacity: 0 }} exit={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.4 }}>
 
-                <h1 className='text-[#888] text-xl mb-10'>About me</h1>
-                <div className='flex flex-col gap-y-5 text-lg text-[#fff]  leading-7 font-light'>
+
+            <h1 className='text-[#888] text-xl mb-10 overflow-hidden' >
+                <motion.p initial={{ opacity: 0, translateX: "20%" }}
+                    animate={{ opacity: 1, translateX: "0%" }}
+                    transition={{
+                        delay: 0.2,
+                        duration: 0.6,
+                        ease: "anticipate"
+                    }}>
+                    About me
+                </motion.p>
+
+            </h1>
+            <div 
+                className=' overflow-hidden'>
+                <motion.div className='flex flex-col gap-y-5 text-lg text-[#fff]  leading-7 font-light' initial={{ opacity: 0, translateY: "40%" }}
+                    animate={{ opacity: 1, translateY: "0%" }}
+                    transition={{
+                        delay: 0.4,
+                        duration: 0.6,
+                        ease: "anticipate"
+                    }}>
                     <p >
                         With over 3 years of experience, I am a seasoned fullstack developer, with relentless pursuit for excellence and an insatiable curiosity for emerging technologies.
                         I possess expertise in the development of intuitive and user-friendly interfaces, dedicated to assisting businesses and individuals in the realization of their objectives.
                     </p>
 
                     <p>As a seasoned fullstack developer, I have an expertise level in both Javascript and PHP,  allowing me to build complex and secure systems while providing user-centric interface.   My distinguished track record, (as you will see in the <Link to={"/projects"}>Projects Section</Link>) for building innovative solutions for individuals, small and large business entities and various stake holders.</p>
+                </motion.div>
+            </div>
+
+
+            <div className='w-10 h-5 border-l border-b border-[#666] my-10'></div>
+
+            <h1 className='text-[#888] text-xl mb-10'>Skills</h1>
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-y-10'>
+                <div className='text-white'>
+                    <h1 className='mb-5'>Languages</h1>
+                    {
+                        skills.languages.map((skill, index) => <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 0.1 }}>
+                            <skill.icon />
+                            <span>{skill.title}</span>
+                        </motion.div>)
+                    }
+
+
                 </div>
-
-
-                <div className='w-10 h-5 border-l border-b border-[#666] my-10'></div>
-
-                <h1 className='text-[#888] text-xl mb-10'>Skills</h1>
-
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-y-10'>
-                    <div className='text-white'>
-                        <h1 className='mb-5'>Languages</h1>
-                        {
-                            skills.languages.map((skill, index) => <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 0.1 }}>
-                                <skill.icon />
-                                <span>{skill.title}</span>
-                            </motion.div>)
-                        }
-
-
+                <div className='text-white col-span-2'>
+                    <h1 className='mb-5'>Frameworks & Others</h1>
+                    <div className='grid grid-cols-2 md:grid-cols-3 gap-x-20 gap-y-5'>
+                        <div>
+                            {
+                                skills.frameworks.map((skill, index) => index + 1 <= 4 && <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }}
+                                >
+                                    <skill.icon />
+                                    <span>{skill.title}</span>
+                                </motion.div>)
+                            }
+                        </div>
+                        <div>
+                            {
+                                skills.frameworks.map((skill, index) => index + 1 > 4 && index + 1 <= 8 && <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 0.1 }}>
+                                    <skill.icon />
+                                    <span>{skill.title}</span>
+                                </motion.div>)
+                            }
+                        </div>
+                        <div>
+                            {
+                                skills.frameworks.map((skill, index) => index + 1 > 8 && index + 1 <= 12 && <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 0.1 }}>
+                                    <skill.icon />
+                                    <span>{skill.title}</span>
+                                </motion.div>)
+                            }
+                        </div>
                     </div>
-                    <div className='text-white col-span-2'>
-                        <h1 className='mb-5'>Frameworks & Others</h1>
-                        <div className='grid grid-cols-2 md:grid-cols-3 gap-x-20 gap-y-5'>
-                            <div>
-                                {
-                                    skills.frameworks.map((skill, index) => index + 1 <= 4 && <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }}
-                                    >
-                                        <skill.icon />
-                                        <span>{skill.title}</span>
-                                    </motion.div>)
-                                }
+
+
+                </div>
+            </div>
+
+            <div className='w-10 h-5 border-l border-b border-[#666] my-10'></div>
+
+            <h1 className='text-[#888] text-xl mb-10'>Professional Experience</h1>
+
+            <div>
+                {
+                    experiences.map((experience, index) => (
+                        <div key={index}>
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 text-white font-Fira " >
+                                <div className='grid grid-cols-12 gap-x-5 '>
+                                    <div className='col-span-4 md:col-span-2'>
+                                        <img src={experience.img} className='object-cover object-center' alt="" />
+                                    </div>
+                                    <div className=' col-span-8 md:col-span-10'>
+                                        <h1 className='text-lg mb-3'>{experience.role}</h1>
+                                        <p className='text-sm text-[#888]'>{experience.company}</p>
+                                        <p className='text-sm text-[#888]'>{experience.date}</p>
+                                    </div>
+                                </div>
+                                <div className=''>
+                                    <h1 className='text-[#888] mb-3'>Roles</h1>
+                                    <ul className='font-light flex flex-col gap-y-5'>
+                                        {
+                                            experience.roles.map((role, index) => <motion.li key={index} initial={{ y: 50, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }}>{role}</motion.li>)
+                                        }
+                                    </ul>
+                                </div>
                             </div>
-                            <div>
-                                {
-                                    skills.frameworks.map((skill, index) => index + 1 > 4 && index + 1 <= 8 && <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 0.1 }}>
-                                        <skill.icon />
-                                        <span>{skill.title}</span>
-                                    </motion.div>)
-                                }
-                            </div>
-                            <div>
-                                {
-                                    skills.frameworks.map((skill, index) => index + 1 > 8 && index + 1 <= 12 && <motion.div className='flex items-center gap-x-3 text-sm mb-5' key={index} initial={{ opacity: 0, x: 50 }} whileInView={{ opacity: 1, x: 0 }} transition={{ delay: 0.1 }}>
-                                        <skill.icon />
-                                        <span>{skill.title}</span>
-                                    </motion.div>)
-                                }
+
+                            <div className='py-20 px-20'>
+                                <div className='h-1 border-b border-b-[#333]'></div>
                             </div>
                         </div>
+                    ))
+                }
+            </div>
 
-
-                    </div>
-                </div>
-
-                <div className='w-10 h-5 border-l border-b border-[#666] my-10'></div>
-
-                <h1 className='text-[#888] text-xl mb-10'>Professional Experience</h1>
-
-                <div>
-                    {
-                        experiences.map((experience, index) => (
-                            <div key={index}>
-                                <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 text-white font-Fira " >
-                                    <div className='grid grid-cols-12 gap-x-5 '>
-                                        <div className='col-span-4 md:col-span-2'>
-                                            <img src={experience.img} className='object-cover object-center' alt="" />
-                                        </div>
-                                        <div className=' col-span-8 md:col-span-10'>
-                                            <h1 className='text-lg mb-3'>{experience.role}</h1>
-                                            <p className='text-sm text-[#888]'>{experience.company}</p>
-                                            <p className='text-sm text-[#888]'>{experience.date}</p>
-                                        </div>
-                                    </div>
-                                    <div className=''>
-                                        <h1 className='text-[#888] mb-3'>Roles</h1>
-                                        <ul className='font-light flex flex-col gap-y-5'>
-                                            {
-                                                experience.roles.map((role, index) => <motion.li key={index} initial={{ y: 50, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }}>{role}</motion.li>)
-                                            }
-                                        </ul>
-                                    </div>
-                                </div>
-
-                                <div className='py-20 px-20'>
-                                    <div className='h-1 border-b border-b-[#333]'></div>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-  
         </motion.div>
     )
 }
